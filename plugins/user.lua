b932cb51d0848361515349b2950da33656c8edfa
René Kulschewski <rene.kulschewski@lekker.de>
2023-04-28 20:18:43 +0200

    ansible support: https://astronvim.com/Recipes/yaml_ansible

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,15 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  -- "andweeb/presence.nvim",
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function()
+  --     require("lsp_signature").setup()
+  --   end,
+  -- },
+  -- { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} },
+  "mfussenegger/nvim-dap",
+  "rcarriga/nvim-dap-ui", 
 }
